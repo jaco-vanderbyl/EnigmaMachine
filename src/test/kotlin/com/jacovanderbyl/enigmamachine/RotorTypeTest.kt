9de0c6786f427891c66cac7b6b4ebab5acc646c4
Jaco van der Byl <jaco.vanderbyl@gmail.com>
2023-12-05 17:44:13 +0000

    Clean RotorTypeTest.

--- src/test/kotlin/com/jacovanderbyl/enigmamachine/RotorTypeTest.kt
+++ src/test/kotlin/com/jacovanderbyl/enigmamachine/RotorTypeTest.kt
@@ -1,98 +1,100 @@
 package com.jacovanderbyl.enigmamachine
 
+import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 class RotorTypeTest {
-    private val rotorCipherSets = mapOf(
-        RotorType.I to "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
-        RotorType.II to "AJDKSIRUXBLHWTMCQGZNPYFVOE",
-        RotorType.III to "BDFHJLCPRTXVZNYEIWGAKMUSQO",
-        RotorType.IV to "ESOVPZJAYQUIRHXLNFTGKDCMWB",
-        RotorType.V to "VZBRGITYUPSDNHLXAWMJQOFECK",
-        RotorType.VI to "JPGVOUMFYQBENHZRDKASXLICTW",
-        RotorType.VII to "NZJHGRCXMYSWBOUFAIVLPEKQDT",
-        RotorType.VIII to "FKQHTLXOCBJSPDZRAMEWNIUYGV",
-    )
+    private fun expectedCipherSets(rotorType: RotorType) = when (rotorType) {
+        RotorType.I -> "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+        RotorType.II -> "AJDKSIRUXBLHWTMCQGZNPYFVOE"
+        RotorType.III -> "BDFHJLCPRTXVZNYEIWGAKMUSQO"
+        RotorType.IV -> "ESOVPZJAYQUIRHXLNFTGKDCMWB"
+        RotorType.V -> "VZBRGITYUPSDNHLXAWMJQOFECK"
+        RotorType.VI -> "JPGVOUMFYQBENHZRDKASXLICTW"
+        RotorType.VII -> "NZJHGRCXMYSWBOUFAIVLPEKQDT"
+        RotorType.VIII -> "FKQHTLXOCBJSPDZRAMEWNIUYGV"
+        else -> throw IllegalArgumentException()
+    }
 
-    private val rotorNotchPositions = mapOf(
-        RotorType.I to setOf('Q'),
-        RotorType.II to setOf('E'),
-        RotorType.III to setOf('V'),
-        RotorType.IV to setOf('J'),
-        RotorType.V to setOf('Z'),
-        RotorType.VI to setOf('Z', 'M'),
-        RotorType.VII to setOf('Z', 'M'),
-        RotorType.VIII to setOf('Z', 'M'),
-    )
+    private fun expectedNotchPositions(rotorType: RotorType) = when (rotorType) {
+        RotorType.I -> setOf('Q')
+        RotorType.II -> setOf('E')
+        RotorType.III -> setOf('V')
+        RotorType.IV -> setOf('J')
+        RotorType.V -> setOf('Z')
+        RotorType.VI -> setOf('Z', 'M')
+        RotorType.VII -> setOf('Z', 'M')
+        RotorType.VIII -> setOf('Z', 'M')
+        else -> throw IllegalArgumentException()
+    }
 
-    private val rotorCompatibility = mapOf(
-        RotorType.I to setOf(EnigmaType.ENIGMA_I, EnigmaType.ENIGMA_M3),
-        RotorType.II to setOf(EnigmaType.ENIGMA_I, EnigmaType.ENIGMA_M3),
-        RotorType.III to setOf(EnigmaType.ENIGMA_I, EnigmaType.ENIGMA_M3),
-        RotorType.IV to setOf(EnigmaType.ENIGMA_I, EnigmaType.ENIGMA_M3),
-        RotorType.V to setOf(EnigmaType.ENIGMA_I, EnigmaType.ENIGMA_M3),
-        RotorType.VI to setOf(EnigmaType.ENIGMA_M3),
-        RotorType.VII to setOf(EnigmaType.ENIGMA_M3),
-        RotorType.VIII to setOf(EnigmaType.ENIGMA_M3),
-    )
+    private fun expectedCompatibility(rotorType: RotorType) = when (rotorType) {
+        RotorType.I -> setOf(EnigmaType.ENIGMA_I, EnigmaType.ENIGMA_M3)
+        RotorType.II -> setOf(EnigmaType.ENIGMA_I, EnigmaType.ENIGMA_M3)
+        RotorType.III -> setOf(EnigmaType.ENIGMA_I, EnigmaType.ENIGMA_M3)
+        RotorType.IV -> setOf(EnigmaType.ENIGMA_I, EnigmaType.ENIGMA_M3)
+        RotorType.V -> setOf(EnigmaType.ENIGMA_I, EnigmaType.ENIGMA_M3)
+        RotorType.VI -> setOf(EnigmaType.ENIGMA_M3)
+        RotorType.VII -> setOf(EnigmaType.ENIGMA_M3)
+        RotorType.VIII -> setOf(EnigmaType.ENIGMA_M3)
+        else -> throw IllegalArgumentException()
+    }
 
-    @Test
-    fun `ensure factory-created rotors are created with correct configuration`() {
-        RotorType.entries.forEach { rotorType ->
-            require(rotorCipherSets.containsKey(rotorType))
-            require(rotorNotchPositions.containsKey(rotorType))
-            require(rotorCompatibility.containsKey(rotorType))
-
+    @TestFactory
+    fun `ensure factory creates rotors correctly`() = RotorType.entries.map { rotorType ->
+        DynamicTest.dynamicTest("Test factory creation of rotor type: '${rotorType}'.") {
             val rotor = rotorType.create()
 
-            Enigma.CHARACTER_SET.forEachIndexed { index, character ->
+            Enigma.CHARACTER_SET.forEachIndexed { index, char ->
                 assertEquals(
-                    expected = rotorCipherSets[rotorType]?.getOrNull(index),
-                    actual = rotor.encipher(character),
-                    message = "Failed to ensure factory-created rotor '${rotorType}' enciphered correctly."
+                    expected = expectedCipherSets(rotorType)[index],
+                    actual = rotor.encipher(char),
+                    message = "Failed to ensure factory creates rotor that enciphers correctly."
                 )
             }
 
             assertEquals(
                 expected = rotorType,
                 actual = rotor.type,
-                message = "Failed to ensure factory-created rotor '${rotorType}' has correct type."
+                message = "Failed to ensure factory creates rotor with correct type."
             )
 
-            rotorCompatibility[rotorType]?.forEach { enigmaType ->
-                assertTrue(
+            EnigmaType.entries.forEach { enigmaType ->
+                assertEquals(
+                    expected = enigmaType in expectedCompatibility(rotorType),
                     actual = rotor.isCompatible(enigmaType),
-                    message = "Failed to ensure factory-created rotor '${rotorType}' has correct compatibility."
+                    message = "Failed to ensure factory creates rotor with correct compatibility."
                 )
             }
 
-            rotorNotchPositions[rotorType]?.forEach { notchPosition ->
-                rotor.position = Position(notchPosition)
-                assertTrue(
+            Enigma.CHARACTER_SET.forEach { position ->
+                rotor.position = Position(position)
+                assertEquals(
+                    expected = position in expectedNotchPositions(rotorType),
                     actual = rotor.isInNotchedPosition(),
-                    message = "Failed to ensure factory-created rotor '${rotorType}' has correct notch characters."
+                    message = "Failed to ensure factory creates rotor with correct notch positions."
                 )
             }
         }
     }
 
     @Test
-    fun `ensure factory-created rotors are created correctly with given position and ring setting`() {
+    fun `ensure factory creates rotors correctly given position and ring setting`() {
         RotorType.entries.forEach { rotorType ->
             val rotor = rotorType.create(Position('L'), RingSetting(24))
 
             assertEquals(
                 expected = 'L',
                 actual = rotor.position.character,
-                message = "Failed to ensure rotor '${rotorType}' is created with given position."
+                message = "Failed to ensure factory creates rotor correctly given position."
             )
 
             assertEquals(
                 expected = 24,
                 actual = rotor.ringSetting.value,
-                message = "Failed to ensure rotor '${rotorType}' is created with given ring setting."
+                message = "Failed to ensure factory creates rotor correctly given ring setting."
             )
         }
     }
